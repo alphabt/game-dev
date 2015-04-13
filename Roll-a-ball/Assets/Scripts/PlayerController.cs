b9c7b79766ef3f0de68db0f07d037895f6c4da77
Brian Tai <iczman@icz-mac.local>
2015-04-13 19:11:39 +0800

    Added Roll-a-ball and Space Shooter Unity tutorials

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start ()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12)
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+}
