b5390335343c2bc6205931588e395035f989ae2d
iczman <alphabt@gmail.com>
2015-04-22 17:11:20 +0800

    Added game over and restart states

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -12,13 +12,33 @@ public class GameController : MonoBehaviour
 	public float waveWait;
 
 	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+
+	private bool gameOver;
+	private bool restart;
 	private int score;
 
 	void Start ()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = string.Empty;
+		gameOverText.text = string.Empty;
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
 	}
 
 	IEnumerator SpawnWaves ()
@@ -36,6 +56,13 @@ public class GameController : MonoBehaviour
 			}
 
 			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' to restart";
+				restart = true;
+				break;
+			}
 		}
 	}
 
@@ -48,5 +75,11 @@ public class GameController : MonoBehaviour
 	void UpdateScore ()
 	{
 		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
 	}
 }
