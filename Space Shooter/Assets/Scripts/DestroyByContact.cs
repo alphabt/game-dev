2f45136c4c201481071ea9be87cd734c2b9eb88c
iczman <alphabt@gmail.com>
2015-04-22 16:47:49 +0800

    Added score UI

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -5,19 +5,37 @@ public class DestroyByContact : MonoBehaviour
 {
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Can't find 'GameController' script");
+		}
+	}
 
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.tag == "Boundary") {
+		if (other.tag == "Boundary")
+		{
 			return;
 		}
 
 		Instantiate (explosion, transform.position, transform.rotation);
 
-		if (other.tag == "Player") {
+		if (other.tag == "Player")
+		{
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 		}
 
+		gameController.AddScore(scoreValue);
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
