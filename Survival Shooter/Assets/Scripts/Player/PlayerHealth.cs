8e63110e3a821416f5d47a79eee6cd0da7516446
iczman <alphabt@gmail.com>
2015-05-15 15:00:24 +0800

    Added ability to harm enemy

--- Survival Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Survival Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,82 +4,71 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int startingHealth = 100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image damageImage;
-    public AudioClip deathClip;
-    public float flashSpeed = 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public AudioClip deathClip;
+	public float flashSpeed = 5f;
+	public Color flashColour = new Color (1f, 0f, 0f, 0.1f);
+	Animator anim;
+	AudioSource playerAudio;
+	PlayerMovement playerMovement;
+	PlayerShooting playerShooting;
+	bool isDead;
+	bool damaged;
 
+	void Awake ()
+	{
+		anim = GetComponent <Animator> ();
+		playerAudio = GetComponent <AudioSource> ();
+		playerMovement = GetComponent <PlayerMovement> ();
+		playerShooting = GetComponentInChildren <PlayerShooting> ();
+		currentHealth = startingHealth;
+	}
 
-    Animator anim;
-    AudioSource playerAudio;
-    PlayerMovement playerMovement;
-    //PlayerShooting playerShooting;
-    bool isDead;
-    bool damaged;
+	void Update ()
+	{
+		if (damaged) {
+			damageImage.color = flashColour;
+		} else {
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
 
+	public void TakeDamage (int amount)
+	{
+		damaged = true;
 
-    void Awake ()
-    {
-        anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();
-        currentHealth = startingHealth;
-    }
+		currentHealth -= amount;
 
+		healthSlider.value = currentHealth;
 
-    void Update ()
-    {
-        if(damaged)
-        {
-            damageImage.color = flashColour;
-        }
-        else
-        {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
-        damaged = false;
-    }
+		playerAudio.Play ();
 
+		if (currentHealth <= 0 && !isDead) {
+			Death ();
+		}
+	}
 
-    public void TakeDamage (int amount)
-    {
-        damaged = true;
+	void Death ()
+	{
+		isDead = true;
 
-        currentHealth -= amount;
+		playerShooting.DisableEffects ();
 
-        healthSlider.value = currentHealth;
+		anim.SetTrigger ("Die");
 
-        playerAudio.Play ();
+		playerAudio.clip = deathClip;
+		playerAudio.Play ();
 
-        if(currentHealth <= 0 && !isDead)
-        {
-            Death ();
-        }
-    }
+		playerMovement.enabled = false;
+		playerShooting.enabled = false;
+	}
 
-
-    void Death ()
-    {
-        isDead = true;
-
-        //playerShooting.DisableEffects ();
-
-        anim.SetTrigger ("Die");
-
-        playerAudio.clip = deathClip;
-        playerAudio.Play ();
-
-        playerMovement.enabled = false;
-        //playerShooting.enabled = false;
-    }
-
-
-    public void RestartLevel ()
-    {
-        Application.LoadLevel (Application.loadedLevel);
-    }
+	public void RestartLevel ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
 }
