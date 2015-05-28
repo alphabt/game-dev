dfd78c55a8abadd2cf2f00ad00c81fcbeab53e5d
iczman <alphabt@gmail.com>
2015-05-28 10:34:42 +0800

    Ch 2.4 Player Movement

---
+++ Stealth/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public AudioClip shoutingClip;
+	public float turnSmoothing = 15f;
+	public float speedDampTime = 0.1f;
+
+	private Animator anim;
+	private HashIDs hash;
+	private Rigidbody rigidbody;
+	private AudioSource audio;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+		hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
+		anim.SetLayerWeight (1, 1f);
+		rigidbody = GetComponent<Rigidbody>();
+		audio = GetComponent<AudioSource>();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		bool sneak = Input.GetButton ("Sneak");
+
+		MovementManagement(h, v, sneak);
+	}
+
+	void Update()
+	{
+		bool shout = Input.GetButtonDown ("Attract");
+		anim.SetBool (hash.shoutingBool, shout);
+		AudioManagement(shout);
+	}
+
+	void MovementManagement(float horizontal, float vertical, bool sneaking)
+	{
+		anim.SetBool (hash.sneakingBool, sneaking);
+
+		if (horizontal != 0f || vertical != 0f)
+		{
+			Rotation (horizontal, vertical);
+			anim.SetFloat (hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
+		}
+		else
+		{
+			anim.SetFloat (hash.speedFloat, 0f);
+		}
+	}
+
+	void Rotation(float horizontal, float vertical)
+	{
+		Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+		Quaternion targetQuaternion = Quaternion.LookRotation (targetDirection, Vector3.up);
+		Quaternion newRotation = Quaternion.Lerp (rigidbody.rotation, targetQuaternion, turnSmoothing * Time.deltaTime);
+		rigidbody.MoveRotation(newRotation);
+	}
+
+	void AudioManagement(bool shout)
+	{
+		if (anim.GetCurrentAnimatorStateInfo(0).shortNameHash == hash.locomotionState)
+		{
+			if (!audio.isPlaying)
+			{
+				audio.Play();
+			}
+		}
+		else
+		{
+			audio.Stop();
+		}
+
+		if (shout)
+		{
+			AudioSource.PlayClipAtPoint(shoutingClip, transform.position);
+		}
+	}
+}
