b823028223ab23f567856850bf3ed6bf99d394bf
iczman <alphabt@gmail.com>
2015-07-02 19:43:36 +0800

    Added touch control

--- Pong/Assets/Scripts/PlayerMovement.cs
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public Vector2 playerResetPos;
-    
     private Rigidbody2D rigidbody2d;
     private GameController gameController;
     private GameObject board;
@@ -20,12 +19,32 @@ public class PlayerMovement : MonoBehaviour
         playerYMin = playerYMax * -1f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (!gameController.gameOver)
         {
+            // Touch Control
+            foreach (Touch touch in Input.touches)
+            {
+                if ((touch.position.x > Screen.width / 2 && this.name.Equals("Player1")) ||
+                    (touch.position.x <= Screen.width / 2 && this.name.Equals("Player2")))
+                {
+                    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                    
+                    rigidbody2d.position = new Vector2
+                    (
+                        rigidbody2d.position.x,
+                        Mathf.Clamp(touchPosition.y, playerYMin, playerYMax)
+                    );
+                }
+            }
+
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+
+            // Keyboard Control
             int moveVertical;
-            if(gameObject.name.Equals("Player1"))
+            
+            if (gameObject.name.Equals("Player1"))
             {
                 moveVertical = (int)Input.GetAxisRaw("Vertical1");
             }
@@ -33,14 +52,16 @@ public class PlayerMovement : MonoBehaviour
             {
                 moveVertical = (int)Input.GetAxisRaw("Vertical2");
             }
-
+            
             float newYPos = rigidbody2d.position.y + (moveVertical * speed / 100f);
-
+            
             rigidbody2d.position = new Vector2
             (
                 rigidbody2d.position.x,
                 Mathf.Clamp(newYPos, playerYMin, playerYMax)
             );
+#endif
+
         }
     }
 }
